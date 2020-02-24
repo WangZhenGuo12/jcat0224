37dbd01e1ad2c365b028326ff2b4a5a49444dffa
wangzhenguo <wangzhenguo302108@163.com>
2020-02-24 17:11:24 +0800

    first init jcat adminstration back

---
+++ jcat-adminstration-back/src/main/java/com/wzg/jcatadminstrationback/JcatAdminstrationBackApplication.java
@@ -0,0 +1,13 @@
+package com.wzg.jcatadminstrationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcatAdminstrationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcatAdminstrationBackApplication.class, args);
+    }
+
+}
---
+++ jcat-adminstration-back/src/test/java/com/wzg/jcatadminstrationback/JcatAdminstrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzg.jcatadminstrationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcatAdminstrationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
