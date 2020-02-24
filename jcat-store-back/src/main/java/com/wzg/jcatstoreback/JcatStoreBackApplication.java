d09daa12d1273007b62269fd8670f876935e487b
wangzhenguo <wangzhenguo302108@163.com>
2020-02-24 17:14:10 +0800

    first init jcat store back

---
+++ jcat-store-back/src/main/java/com/wzg/jcatstoreback/JcatStoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.wzg.jcatstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcatStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcatStoreBackApplication.class, args);
+    }
+
+}
---
+++ jcat-store-back/src/test/java/com/wzg/jcatstoreback/JcatStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzg.jcatstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcatStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
